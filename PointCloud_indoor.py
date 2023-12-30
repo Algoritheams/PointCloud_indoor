d24c68e575ace55f8732badb1be5940d6a0471dc
efebasar14 <107487212+efebasar14@users.noreply.github.com>
2023-12-30 15:34:38 +0300

    Update PointCloud_indoor.py
    I've added python library

--- PointCloud_indoor.py
+++ PointCloud_indoor.py
@@ -13,5 +13,31 @@ def read_file (file):
       return read
 except yaml. YAMLError as exc:
 print (exc) 
+import math
+
+class LidarScan:
+    def __init__(self, num_points=360, radius=10):
+        self.num_points = num_points
+        self.radius = radius
+        self.points = []
+
+    def generate_scan(self):
+        self.points = []
+        angle_increment = 360 / self.num_points
+
+        for i in range(self.num_points):
+            angle = math.radians(i * angle_increment)
+            x = self.radius * math.cos(angle)
+            y = self.radius * math.sin(angle)
+            self.points.append((x, y))
+
+    def print_scan(self):
+        for point in self.points:
+            print(f"Point: {point}")
+
+# Example usage:
+lidar_scan = LidarScan()
+lidar_scan.generate_scan()
+lidar_scan.print_scan()
 
 
